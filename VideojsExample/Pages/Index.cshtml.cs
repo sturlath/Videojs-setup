8f274995ae188779df43e8ea9fd75e83ea65f5c9
Sturla Thorvaldsson <sturla@ibeinni.is>
2022-08-28 12:53:54 +0000

    Add project files.

---
+++ VideojsExample/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VideojsExample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [HiddenInput]
+        [BindProperty(SupportsGet = true)]
+        public string ManifestUrlHidden { get; set; }
+
+        [HiddenInput]
+        [BindProperty(SupportsGet = true)]
+        public string TokenHidden { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            //DASH
+            //ManifestUrlHidden = "https://beinnip-euno.streaming.media.azure.net/f76256b1-d120-406d-af06-4eea119c509b/dd6f798e-3e73-4e8b-96fe-789548854387.ism/manifest(format=mpd-time-cmaf,encryption=cbc)";
+
+            //HLS
+            ManifestUrlHidden = "https://beinnip-euno.streaming.media.azure.net/f76256b1-d120-406d-af06-4eea119c509b/dd6f798e-3e73-4e8b-96fe-789548854387.ism/manifest(format=m3u8-cmaf,encryption=cbc)";
+
+            //
+            var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cm46bWljcm9zb2Z0OmF6dXJlOm1lZGlhc2VydmljZXM6Y29udGVudGtleWlkZW50aWZpZXIiOiI1MjI5YjkzOC1kYWFlLTQwNWUtOGMzMy00YjViZTU0ZmRjZmEiLCJ1cm46bWljcm9zb2Z0OmF6dXJlOm1lZGlhc2VydmljZXM6bWF4dXNlcyI6IjUiLCJuYmYiOjE2NjE2OTA3MjMsImV4cCI6MTY2MTY5MTYyMywiaXNzIjoiaUJlaW5uaUlzc3VlciIsImF1ZCI6ImlCZWlubmlBdWRpZW5jZSJ9.IQ1hplAANtdGtC7scL6zzRzh7UBQPHr37qkCrQOuiTw";
+            TokenHidden = $"Bearer={token}";
+        }
+    }
+}
---
+++ VideojsExample/Pages/Index.cshtml.js
@@ -0,0 +1,211 @@
+﻿
+const Types = {
+    hlsType: 'application/x-mpegURL',
+    dashType: 'application/dash+xml',
+    dashCmaf: 'mpd-time-cmaf'
+}
+
+const colors = {
+    DEBUG: 'text-black',
+    INFO: 'text-green-500',
+    WARNING: 'text-yellow-500',
+    ERROR: 'text-red-500'
+}
+
+class VideojsPlayer {
+
+    initPlayer() {
+
+        var options = {
+            html5: {
+                vhs: {
+                    // Try to use videojs-http-streaming even on platforms that provide some level of HLS support natively. 
+                    // There are a number of platforms that technically play back HLS content but aren't very reliable or are
+                    //  missing features like CEA-608 captions support. When overrideNative is true, if the platform supports 
+                    // Media Source Extensions videojs-http-streaming will take over HLS playback to provide a more consistent experience. 
+                    // NOTE: This includes Edge browser. It has a native HLS player that will stall out if you do not do this.
+                    overrideNative: true
+                },
+                nativeAudioTracks: false,
+                nativeVideoTracks: false
+            }
+        };
+ 
+        const videoJS = videojs('video', options, function onPlayerReady() {
+            videojs.log('Your player is ready!');
+
+            // How about an event listener?
+            this.on('ended', function () {
+                videojs.log('Awww...over so soon?!');
+            });
+        });
+
+        const subtitleKind = 'subtitles'
+        const subtitleLang = 'eng'
+        const subtitleLabel = 'test'
+
+        videojs.Vhs.xhr.beforeRequest = this.setupTokenForDecrypt.bind(this)
+
+        let typeUrl = Types.hlsType
+        // Get a handle to the VHS tech for HLS playback. See - https://github.com/videojs/http-streaming
+        var vhs = videoJS.tech().vhs;
+
+        if (this.format === 'dash') {
+            typeUrl = Types.dashType
+        }
+
+        videoJS.eme()
+
+        if (this.playReadyLicenseUrl || this.widevineLicenseUrl || this.fairPlayCertificate) {
+            videoJS.src({
+                src: this.getManifest(),
+                type: typeUrl,
+                emeHeaders: { Authorization: 'Bearer=' + this.getInputToken() },
+                keySystems: {
+                    'com.microsoft.playready': this.playReadyLicenseUrl,
+                    'com.widevine.alpha': this.widevineLicenseUrl,
+                    'com.apple.fps.1_0': {
+                        certificateUri: this.fairPlayCertificate,
+                        licenseUri: this.fairPlayLicenseUrl
+                    }
+                }
+            })
+        } else {
+            videoJS.src({
+                src: this.getManifest(),
+                type: typeUrl
+            })
+        }
+
+        const logLevelDetailsChosen = 1
+
+        if (logLevelDetailsChosen >= 1) {
+            videojs.log.error = this.interceptLog('ERROR')
+        }
+
+        if (logLevelDetailsChosen >= 2) {
+            videojs.log.warn = this.interceptLog('WARNING')
+        }
+
+        if (logLevelDetailsChosen >= 3) {
+            videojs.log.info = this.interceptLog('INFO')
+        }
+
+        if (logLevelDetailsChosen >= 4) {
+            videojs.log.debug = this.interceptLog('DEBUG')
+        }
+
+        if (this.caption) {
+            videojs.players.video.addRemoteTextTrack({
+                kind: subtitleKind,
+                src: this.caption,
+                srclang: subtitleLang,
+                label: subtitleLabel
+            })
+        };
+    }
+
+    setupTokenForDecrypt(options) {
+        const urlLocation = this.getLocation(options.uri)
+        var authentication = false
+
+        if (this.encryptioKeyUrl && this.encryptioKeyUrl.indexOf(urlLocation.hostname) !== -1) {
+
+            authentication = true
+        }
+        if (this.widevineLicenseUrl && this.widevineLicenseUrl.indexOf(urlLocation.hostname) !== -1) {
+            authentication = true
+        }
+        if (this.fairPlayLicenseUrl && this.fairPlayLicenseUrl.indexOf(urlLocation.hostname) !== -1) {
+            authentication = true
+        }
+        if (this.playReadyLicenseUrl && this.playReadyLicenseUrl.indexOf(urlLocation.hostname) !== -1) {
+            authentication = true
+        }
+
+        if (authentication || options.uri.includes("keydeliver")) {
+            videojs.log("sadf");
+            options.headers = options.headers || {}
+            options.headers.Authorization = 'Bearer=' + this.getInputToken()
+        }
+        return options
+    }
+
+
+    /* from common.js */
+
+    getManifest() {
+        return document.getElementById("ManifestUrlHidden").value;
+    }
+
+    getInputToken() {
+        return document.getElementById("TokenHidden");
+    }
+
+    // override method
+    getPlayerInfo() {
+        return `Video.js v${videojs.VERSION}`
+    }
+
+    // override method
+    getPluginsInfo() {
+        const plugins = []
+        plugins['videojs-contrib-eme'] = 'v4.0.0'
+        return plugins
+    }
+
+    interceptLog(type) {
+        return function () {
+            this.addMessage(type, Array.from(arguments).filter(Boolean).join(' '))
+        }.bind(this)
+    }
+
+    getLocation(href) {
+        const l = document.createElement('a')
+        l.href = href
+        return l
+    }
+
+    getPlayerInfo() {
+        return ''
+    }
+
+    addErrorMessage(message) {
+        this.addMessage('ERROR', message)
+    }
+
+    addWarningMessage(message) {
+        if (this.logLevel > 1) {
+            this.addMessage('WARNING', message)
+        }
+    }
+
+    addInfoMessage(message) {
+        if (this.logLevel > 2) {
+            this.addMessage('INFO', message)
+        }
+    }
+
+    addDebugMessage(message) {
+        if (this.logLevel > 3) {
+            this.addMessage('DEBUG', message)
+        }
+    }
+
+    addMessage(type, message) {
+        if (!message) {
+            return
+        }
+
+        const log = document.createElement('p')
+        log.classList.add(colors[type])
+
+        const now = new Date()
+        log.textContent += now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds() + '.' + now.getMilliseconds() + ' '
+        log.textContent += '[' + type + '] '
+        log.textContent += message
+    }
+}
+
+const videojsPlayer = new VideojsPlayer()
+videojsPlayer.initPlayer()
